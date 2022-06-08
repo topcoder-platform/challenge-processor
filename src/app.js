137193953d67abbbcccec266e27f388af4862ee0
Marios Kranitsas <marioskranitsas1999@gmail.com>
2022-06-08 14:40:52 +0300

    Modify processor to give Ketz edit privileges for any self-service data science intake challenge

--- src/app.js
+++ src/app.js
@@ -49,6 +49,9 @@ const dataHandler = (messageSet, topic, partition) => Promise.each(messageSet, a
       case config.RESOURCE_DELETE_TOPIC:
         await ProcessorService.deleteResource(messageJSON)
         break
+      case config.RESOURCE_CREATED_TOPIC:
+        await ProcessorService.handleChallengeCreation(messageJSON)
+        break
       default:
         throw new Error(`Invalid topic: ${topic}`)
     }
